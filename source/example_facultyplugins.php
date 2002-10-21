46bf61a354b9b29d4f6bf16f4b191a2e94334f3b
basmati <basmati>
2002-10-21 16:20:19 +0000

    Allowing for the use of plugins in the faculty system.

---
+++ source/example_facultyplugins.php
@@ -0,0 +1,34 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Basmati -- Version 2.0P                                              |
+// +----------------------------------------------------------------------+
+// | Copyright (C) 2000-2001 James B. Bassett (basmatisoftware@msn.com)   |
+// +----------------------------------------------------------------------+
+// | This program is free software.  You can redistribute in and/or       |
+// | modify it under the terms of the GNU General Public License Version  |
+// | 2 as published by the Free Software Foundation.                      |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY, without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program;  If not, write to the Free Software         |
+// | Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.            |
+// +----------------------------------------------------------------------+
+// | Authors: James B. Bassett - basmatisoftware@msn.com                  |
+// +----------------------------------------------------------------------+
+//
+// $Id: example_facultyplugins.php,v 1.1 2002/10/21 16:20:19 basmati Exp $
+
+// This file will permit the use of "facultyplugins" added to the bottom
+// of the facultylogin.php file provided that the $enablefacultyplugins 
+// variable in basmaticonstants.php is set to a value of 1.
+// 
+// You should rename this file from "example_facultyplugins.php" to "facultyplugins.php"
+// so that CVS updates do not overwrite your settings.
+
+
+?>
